23ca27a245c326fa66be53932325845f20cac602
Rodrigo Torres <rodrigotorresmessenzani@hotmail.com>
2022-05-04 01:21:18 -0300

    img commit

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,11 +1,20 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
 import G from '../assets/G.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
+
+import gallery01 from '../assets/gallery01.jpg';
+import gallery02 from '../assets/gallery02.jpg';
+import gallery03 from '../assets/gallery03.jpg';
+import gallery04 from '../assets/gallery04.jpg';
+import gallery05 from '../assets/gallery05.jpg';
+import gallery06 from '../assets/gallery06.jpg';
+import gallery07 from '../assets/gallery07.jpg';
+import gallery08 from '../assets/gallery08.jpg';
+import gallery09 from '../assets/gallery09.jpg';
+
+
 import knife from '../assets/knife.png';
+import milk from '../assets/milk.png'
 import logo from '../assets/logo.png';
 import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
@@ -17,6 +26,8 @@ import award01 from '../assets/award01.png';
 import award02 from '../assets/award02.png';
 import award03 from '../assets/award03.png';
 import award05 from '../assets/award05.png';
+import vaca from '../assets/vaca.png';
+import lecheslaurels from '../assets/lecheslaurels.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
@@ -32,6 +43,11 @@ export default {
   gallery02,
   gallery03,
   gallery04,
+  gallery05,
+  gallery06,
+  gallery07,
+  gallery08,
+  gallery09,
   knife,
   logo,
   menu,
@@ -46,6 +62,9 @@ export default {
   award05,
   sign,
   quote,
+  milk,
+  vaca,
+  lecheslaurels,
   gericht,
   nutricionarg,
   milkpreparativo,
